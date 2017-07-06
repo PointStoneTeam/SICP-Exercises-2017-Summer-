6bcca4e089671a28f2977c902793529b0384c5c2
wzcboss <947191263@qq.com>
2017-07-07 01:31:53 +0800

    update

--- wzc-sicplearning/day6/c3.scm
+++ wzc-sicplearning/day6/c3.scm
@@ -1,5 +1,70 @@
+3.1
 (define (make-accumulator init)
 	(lambda (x) (set! init (+ init x)) init)
 )
 
 (define A (make-accumulator 5))
+
+3.2
+(define (make-monitored f)
+  (let ((acc 0))
+    (lambda (x)
+      (cond
+        ((eq? x 'how-many-calls?) acc)
+        ((eq? x 'reset-count) (set! acc 0))
+        (else (begin (set! acc (+ acc 1))
+                     (f x)))))))
+
+(define 我是一个名称 (make-monitored sqrt))
+
+3.3
+
+(define (make-account balance password)
+	(define (withdraw amount)
+		(if (>= balance amount)
+			(begin (set! balance (- balance amount)) balance)
+			"Insufficient funds")
+	)
+	(define (deposit amount)
+		(set! balance (+ balance amount))
+		balance)
+	(define (dispatch p m)
+		(if (eqv? p password)
+			(cond
+				((eq? m 'withdraw) withdraw)
+				((eq? m 'deposit) deposit)
+				(else (error "Unknown request: MAKE-ACCOUNT" m))
+			)
+			(error "Incorrect password")
+		)
+	)
+	dispatch
+)
+
+3.4 银行账户处理系统
+(define (make-account balance password)
+	(define wrong-password-count 0)
+	(define (withdraw amount)
+		(if (>= balance amount)
+			(begin (set! balance (- balance amount)) balance)
+			"Insufficient funds")
+	)
+	(define (deposit amount)
+		(set! balance (+ balance amount))
+		balance)
+	(define (dispatch p m)
+		(if (eqv? p password)
+			(cond
+				((eq? m 'withdraw) withdraw)
+				((eq? m 'deposit) deposit)
+				(else (error "Unknown request: MAKE-ACCOUNT" m))
+			)
+			(begin (set! wrong-password-count (+ wrong-password-count 1))
+        (if (> wrong-password-count 7) call-the-cops)
+         (error "Incorrect password"))
+		)
+	)
+	dispatch
+)
+
+3.6随机赋值
