190e5b754938871ec38b5b945c782ec7b2be5db1
ZiXi-Yang <1046903596@qq.com>
2017-07-07 09:23:36 +0800

    Delete 1.12Recursion.rkt

--- yzx-sicp/1.12Recursion.rkt
+++
@@ -1,9 +0,0 @@
-#lang scheme
-(define (pascal row col)
-  (cond ((> col row)
-         (error "invalid col value"))
-        ((or (= col 0) (= row col))
-         1)
-        (else (+ (pascal (- row 1) (- col 1)) (pascal (- row 1) col)))))
-(pascal 4 1)
-(pascal 42 22)
