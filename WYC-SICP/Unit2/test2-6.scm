b39c504b3e37b69cd93b7587a1c9ecbf04251008
Jordan <lushan05wyc>
2017-07-10 16:24:24 +0800

    test2-6

---
+++ WYC-SICP/Unit2/test2-6.scm
@@ -0,0 +1,14 @@
+(define +
+    (lambda (m)
+        (lambda (n)
+            (lambda (f)
+                (lambda (x)
+                    (m f (n f x)))))))
+(define one
+    (lambda (f)
+        (lambda (x)
+            (f x))))
+(define two
+    (lambda (f)
+        (lambda (x)
+           (f (f x)))))
