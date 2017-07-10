780cc7fe97856371772dcc89d53f0dc06f7c63bf
ZiXi-Yang <1046903596@qq.com>
2017-07-10 23:49:12 +0800

    Update 2.21

--- yzx-sicp/Unit2/2.21.rkt
+++ yzx-sicp/Unit2/2.21.rkt
@@ -1,11 +1,13 @@
 #lang scheme
 (define (square n)
   (* n n))
+  ;;;first way
 (define (square-list items)
     (if (null? items)
         '()
         (cons (square (car items))
               (square-list (cdr items)))))
+  ;;;second way by map
 (define (square-list1 items)
     (map square items))
 (square-list (list 1 2 3 4))
