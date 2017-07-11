75bb4c24801f79c22f6b3304803dcf3df9893465
findsanpang <944857551@qq.com>
2017-07-11 14:42:44 +0800

    Add files via upload

--- ss-sicp/1.11re.rkt
+++ ss-sicp/1.11re.rkt
@@ -1,5 +1,5 @@
 #lang racket
 (define (count n)
-(cond ((< n 3) n)
-((= n 3) 4)
-(else(+(count (- n 1))  (*(- n 2) 2)  (*(- n 3) 3)))))
+(cond ((< n 3)n)
+      ((= n 3)4)
+      (else (+ (count (- n 1)) (*(- n 2) 2) (*(- n 3) 3)))))
