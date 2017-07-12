9378a6fd516fd2f5f4856ca06629f0675080a891
ZiXi-Yang <1046903596@qq.com>
2017-07-12 11:26:50 +0800

    add 2.28

---
+++ yzx-sicp/Unit2/2.28.rkt
@@ -0,0 +1,33 @@
+#lang scheme
+(define (fringe tree)
+  (cond ((null? tree)
+         '())
+        ((not (pair? tree))
+         (list tree))
+        (else
+         (append (fringe (car tree))
+                 (fringe (cadr tree))))))
+(define x (list (list 1 2) (list 3 4)))
+(fringe x)
+;;;better fringe-tree
+(define (fringe2 tree)
+    (cond ((empty-tree? tree)
+            '())
+          ((leaf? tree)
+            (list tree))
+          (else
+            (append (fringe (left-branch tree))
+                    (fringe (right-branch tree))))))
+
+(define (empty-tree? tree)
+    (null? tree))
+
+(define (leaf? tree)
+    (not (pair? tree)))
+
+(define (left-branch tree)
+    (car tree))
+
+(define (right-branch tree)
+    (cadr tree))         
+        
