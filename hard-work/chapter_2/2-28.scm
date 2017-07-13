da9a20675c2e849c19e37008bc8288796c6af31e
ChenLixiang <lixiang3608@outlook.com>
2017-07-13 22:07:19 +0800

    fix 2-28

--- hard-work/chapter_2/2-28.scm
+++ hard-work/chapter_2/2-28.scm
@@ -1,8 +1,8 @@
 (define (fringe-iter x result)
   (cond ((null? x) result)
-        ((not (pair? (car x))) x)
+        ((not (pair? (car x))) (cons (car x) (fringe-iter (cdr x) '())))
         (else 
-          (fringe-iter (cdr x) 
+           (fringe-iter (cdr x) 
            (append result  (fringe-iter (car x) '()))))))
 
 (define (fringe tree) (fringe-iter tree '()))
